0ed4acd01010c2ccfa3e502b215c40accd99aaeb
Stephan Chang <stchepanhagn@gmail.com>
2014-04-16 12:52:44 -0300

    Initial Commit

---
+++ Starcraft2Soundboard/SCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCSAppDelegate.h
+//  Starcraft2Soundboard
+//
+//  Created by Stephan Chang on 4/16/14.
+//  Copyright (c) 2014 AppWerk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Starcraft2Soundboard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Starcraft2Soundboard
+//
+//  Created by Stephan Chang on 4/16/14.
+//  Copyright (c) 2014 AppWerk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCSAppDelegate class]));
+    }
+}
